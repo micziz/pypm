bb34fdf0cc24d3dddfe71f8ab553bcaba0f53e15
micziz <miczicontent@gmail.com>
2022-05-29 19:18:30 +0200

    IT WORKSSSS

--- src/main.py
+++ src/main.py
@@ -1,6 +1,14 @@
 from os import system
-import requests
+import click
 
 def install(x):
-    print(f"Installing: {x}")
-    system("curl -sL ")
+    system(f"curl -sL https://raw.githubusercontent.com/micziz/pypm/main/script/{x}.sh | sh")
+
+@click.command()
+@click.option('--package', prompt='The Package Name')
+def main(package):
+    click.echo(f"Installing: {package}!")
+    install(package)
+
+
+main()
